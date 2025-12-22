f110b6bcab9c2a2c1f9f7640625585a47309e3c9
maximdsgn-cyber <maximdsgn@gmail.com>
2025-12-22 23:11:10 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Grocery Shop",
+  description: "Online grocery store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
